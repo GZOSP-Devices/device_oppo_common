2d907f48adb484556e044bce6ea108fe8b0d5018
hardwareadictos <hardwareadictos@gmail.com>
2018-07-24 13:02:38 +0200

    oppo-common: disable TARGET_RECOVERY_UPDATER_LIBS
    It conflicts with enchiladas AB_OTA_UPDATER

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -22,6 +22,6 @@ BOARD_HARDWARE_CLASS += \
     hardware/cyanogen/cmhw
 
 # Releasetools
-TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_oppo
+#TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_oppo
 TARGET_RELEASETOOLS_EXTENSIONS := device/oppo/common
 
